da049999fab7c8747ea54c61a46f7e1682d39ddf
martimsoutooo <martimsantos@aluno-1193.wireless.ua.pt>
2025-04-12 17:57:15 +0100

    delete register and login pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Layout from "./components/Layout";
 import LayoutAuth from "./components/LayoutAuth";
-import Register from "./pages/Register.jsx";
-import Login from "./pages/Login.jsx";
 import LayoutInstantiate from "./components/LayoutInstantiate.jsx";
 import Users from "./pages/Users.jsx";
 import Alerts from "./pages/Alerts.jsx";
@@ -20,13 +18,10 @@ import PrivateRoute from "./PrivateRoute.js";
 export default function App() {
   return (
       <ReactKeycloakProvider 
-        authClient={keycloak} 
+        authClient={keycloak}
         initOptions={{
-          onLoad: 'login-required',
           checkLoginIframe: false,
           pkceMethod: 'S256',
-          // Set a more reasonable token refresh time (in seconds)
-          // The default is 5 seconds which is too frequent and causes issues
           tokenRefreshInterval: 60
         }}
       >
@@ -35,23 +30,20 @@ export default function App() {
                   <Route element={<Layout />}>
                       <Route index element={<Home />} />
                   </Route>
-                  <Route element={<LayoutAuth/>}>
-                      <Route  path="register" element={<Register />}/>
-                      <Route  path="login" element={<Login />}/>
-                  </Route>
+                  
                   <Route path="instantiate" element={<PrivateRoute><LayoutInstantiate/></PrivateRoute>}>
                       <Route path="home">
                           <Route path="users" element={<Users />}/>
                           <Route path="sessions" element={<Activities/>}/>
                           <Route path="alerts" element={<Alerts/>}/>
                       </Route>
-                          <Route path="eventmaker">
-                              <Route path="colors" element={<Colors/>}></Route>
-                              <Route path="base-configurations" element={<BaseConfiguration/>}></Route>
-                              <Route path="plugins" element={<Plugins/>}></Route>
-                          </Route>
-                          <Route path="schedule" element={<Schedule/>}></Route>
-                          <Route path="register" element={<ManualRegister/>}></Route>
+                      <Route path="eventmaker">
+                          <Route path="colors" element={<Colors/>}></Route>
+                          <Route path="base-configurations" element={<BaseConfiguration/>}></Route>
+                          <Route path="plugins" element={<Plugins/>}></Route>
+                      </Route>
+                      <Route path="schedule" element={<Schedule/>}></Route>
+                      <Route path="register" element={<ManualRegister/>}></Route>
                   </Route>
               </Routes>
           </Router>
