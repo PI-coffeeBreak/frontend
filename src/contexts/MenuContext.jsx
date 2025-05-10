f824132e214e38158f3b98b70e44543252eb5739
Sebastião Teixeira <steixeira2004@gmail.com>
2025-05-10 01:11:40 +0100

    Solve conflicts

--- src/contexts/MenuContext.jsx
+++ src/contexts/MenuContext.jsx
@@ -3,16 +3,10 @@ import PropTypes from "prop-types";
 import { baseUrl } from "../consts";
 
 // Import all icon libraries
-<<<<<<< HEAD
-=======
-import * as Fa from "react-icons/fa";
-import * as Fi from "react-icons/fi";
->>>>>>> dev
 import * as Ai from "react-icons/ai";
 import * as Bi from "react-icons/bi";
 import * as Bs from "react-icons/bs";
 import * as Ci from "react-icons/ci";
-<<<<<<< HEAD
 import * as Cg from "react-icons/cg";
 import * as Di from "react-icons/di";
 import * as Fa from "react-icons/fa";
@@ -39,19 +33,6 @@ import * as Tb from "react-icons/tb";
 import * as Tfi from "react-icons/tfi";
 import * as Ti from "react-icons/ti";
 import * as Vsc from "react-icons/vsc";
-=======
-import * as Di from "react-icons/di";
-import * as Gi from "react-icons/gi";
-import * as Go from "react-icons/go";
-import * as Hi from "react-icons/hi";
-import * as Im from "react-icons/im";
-import * as Io from "react-icons/io";
-import * as Io5 from "react-icons/io5";
-import * as Md from "react-icons/md";
-import * as Ri from "react-icons/ri";
-import * as Si from "react-icons/si";
-import * as Ti from "react-icons/ti";
->>>>>>> dev
 import * as Wi from "react-icons/wi";
 
 import { axiosWithAuth } from "../utils/axiosWithAuth";
